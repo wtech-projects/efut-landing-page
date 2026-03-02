3af1291f627fcc46fbcef91e61116432e8db8008
Williams Araujo <williamsa@ats4it.com>
2026-03-02 23:08:13 +0100

    fix

--- libs/environment/src/lib/environment.ts
+++ libs/environment/src/lib/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   environmentName: 'Production',
   baseUrl: 'http://localhost:8080',
-  onboardingBaseUrl: 'https://api.gramadosvirtuais/onboarding',
+  onboardingBaseUrl: 'https://api.gramadosvirtuais.com/onboarding',
   humanVerificationSiteKey: '6Legp30sAAAAAFvdyRWxQ6satEAizPSU9oJxl6PY',
 };
